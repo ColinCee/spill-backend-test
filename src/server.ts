81d50b53bf46a792f0be09045914140d1a437210
Colin Cheung <ColinCee@users.noreply.github.com>
2021-12-19 21:10:54 +0000

    FIX bug listening on docker

--- src/server.ts
+++ src/server.ts
@@ -3,10 +3,10 @@ import { buildFastify } from './app';
 const start = async () => {
   const server = buildFastify();
   try {
-    await server.listen(3000);
+    await server.listen(3000, '0.0.0.0');
     const address = server.server.address();
     const port = typeof address === 'string' ? address : address?.port;
-    console.log(port);
+    console.log('Listening on port: ', port);
   } catch (err) {
     server.log.error(err);
     process.exit(1);
